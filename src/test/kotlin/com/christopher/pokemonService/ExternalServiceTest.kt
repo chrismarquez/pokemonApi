a6e573a27fe82d65f1b788775c9f3c2ca76709ce
chrismarquez <christophersmarquez@outlook.com>
2020-07-17 18:37:48 -0500

    Adding CI, Test Suite

---
+++ src/test/kotlin/com/christopher/pokemonService/ExternalServiceTest.kt
@@ -0,0 +1,13 @@
+package com.christopher.pokemonService
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class ExternalServiceTest {
+
+    @Test
+    fun isTestSuiteWorking() {
+        Assertions.assertTrue(true)
+    }
+
+}
