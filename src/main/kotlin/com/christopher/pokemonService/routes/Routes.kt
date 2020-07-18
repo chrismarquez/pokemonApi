dbbd5cb115775dfe22ed0525df7afc8658286fc9
chrismarquez <christophersmarquez@outlook.com>
2020-07-17 21:03:49 -0500

    Setting up Kubernetes

--- src/main/kotlin/com/christopher/pokemonService/routes/Routes.kt
+++ src/main/kotlin/com/christopher/pokemonService/routes/Routes.kt
@@ -1,7 +1,16 @@
 package com.christopher.pokemonService.routes
 
 import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
 
-fun Application.routes() {
-
+fun Application.routes() = routing {
+    route("/") {
+        get {
+            call.respond("I am alive")
+        }
+    }
 }
