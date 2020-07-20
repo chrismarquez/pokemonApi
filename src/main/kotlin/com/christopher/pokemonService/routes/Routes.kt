3b4c37f5e33176810ca7c6cc95e7fc2fd658f171
chrismarquez <christophersmarquez@outlook.com>
2020-07-19 21:20:26 -0500

    feat: change port, add /v1 prefix to api path

--- src/main/kotlin/com/christopher/pokemonService/routes/Routes.kt
+++ src/main/kotlin/com/christopher/pokemonService/routes/Routes.kt
@@ -3,16 +3,23 @@ package com.christopher.pokemonService.routes
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.response.respond
+import io.ktor.response.respondRedirect
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
 
+private const val openApiDocs = "https://app.swaggerhub.com/apis/chrismarquez7/PokemonAPI/1.0.0"
+
 fun Application.routes() = routing {
     route("/") {
         get {
-            call.respond("I am too alive, and I am the Senate")
+            call.respondRedirect(openApiDocs, true)
         }
+    }
+    route("/v1") {
+        route("/common", Route::common)
+        route("/compare", Route::compare)
     }
     route("/common", Route::common)
     route("/compare", Route::compare)
